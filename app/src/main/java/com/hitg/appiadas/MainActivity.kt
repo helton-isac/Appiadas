c8545c461097fb8081c0d1a1795bdc80b393f0f2
Helton Isac <helton.isac@gmail.com>
2020-08-20 22:54:03 -0300

    First Commit

---
+++ app/src/main/java/com/hitg/appiadas/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.hitg.appiadas
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btTellJoker.setOnClickListener {
+            showJoker()
+        }
+
+        showJoker()
+    }
+
+    private fun showJoker() {
+        val jokers = resources.getStringArray(R.array.jokers)
+        val numberJoker = Random().nextInt(jokers.size)
+        val joker = jokers[numberJoker]
+        tvJoker.text = joker
+        playSong()
+    }
+
+    private fun playSong() {
+        val mediaPlayer = MediaPlayer.create(this,R.raw.badumtss)
+        mediaPlayer.start()
+    }
+}
